8293d45aa73a9f4dea77a9286441c0c34ea6fda1
Jasper Smits <jasper.smits@upcmail.nl>
2022-06-10 14:45:38 +0200

    Refined communication protocol. Exposing most laser properties through LaserSettings class.

--- LaserCommunication.py
+++ LaserCommunication.py
@@ -1,21 +1,51 @@
-
-
-
-
+import telnetlib
 
 class LaserCommunication:
-    def __init__(self):
-        # Connect to laser
-        pass
-    
-    def __send_message(self,message):
-        pass
-    
-    def __recv_message(self):
-        message = "Test response"
-        return message
+    """
+    Class to manage communication to a QSmart laser using the telnet protocol.
+
+    This class was designed with the QSmart 450 in mind, but might work for other lasers."""
+
+    def __init__(self,ip,port,timeout):
+        """
+        Initializes the telnet connection. Takes ip, port and timeout keywords to be passed to telnet.
+        """
         
-    def send_and_recv(self,message):
-        self.__send_message(message)
-        return self.__recv_message()
+        ## Initialize the telnet object
+        self.tn = telnetlib.Telnet(ip,port,timeout)
+
+        ## Test the connection, the laser should always respond to STATE
+        try:
+            self.send_and_recv("STATE")
+        except Exception as e:
+            raise IOError("Unable to connect to laser:",e)
+
+    def __send_message(self,message):
+        self.tn.write(message.encode("ascii")+b'\n')
     
+    def __recv_message(self,include_status=False):
+        """Message receiver
+
+        Problem occurs that messages are read too quickly, so we always want to be sure we read something.
+
+        We read up to the first line break. If the message doesn't contain "ERROR", we have to read to another line break."""
+        message = self.tn.read_until(b"\n")
+
+        if b"ERROR" in message or b"OK" in message:
+            ## If there is an error in the message, we can only read one line.
+            pass
+        else:
+            ## If there is no error in the message, the status code will also be passed to us, so we read another line.
+            message += self.tn.read_until(b"\n")
+
+        message = message.decode("ascii")
+        if include_status:
+            return message
+        else:
+            message_parts = message.split("\n")
+            return message_parts[0]
+
+        
+    def send_and_recv(self,message,include_status=False):
+        self.__send_message(message)
+        return self.__recv_message(include_status)
--- LaserSettings.py
+++ LaserSettings.py
@@ -1,6 +1,3 @@
-
-
-
 import LaserCommunication
 
 state_dict = {\
@@ -17,8 +14,8 @@ state_dict = {\
               }
 
 class LaserSettings:
-    def __init__(self,admin_mode = False):
-        self.lasercommunication = LaserCommunication.LaserCommunication()
+    def __init__(self,admin_mode = False,ip="169.254.0.1",port=10001,timeout=3):
+        self.lasercommunication = LaserCommunication.LaserCommunication(ip,port,timeout)
         self.admin_mode = admin_mode
         
         ## Get firmware settings
@@ -36,7 +33,7 @@ class LaserSettings:
             return self.__get_STATE(string=True)
         else:
             return self.__get_STATE()
-            
+
     def enable_flash(self):
         return self.__FL_RUN()
         
@@ -48,17 +45,265 @@ class LaserSettings:
         
     def disable_qsw(self):
         return self.__QS_STOP()
+
+    @property
+    def state(self):
+        """Returns state of the laser as a number."""
+        return self.get_state()
+
+    @property
+    def state_str(self):
+        """Returns state of the laser as a string."""
+        return self.get_state(code=False,string=True)
+
+
+    @property
+    def flashlamp_voltage(self):
+        """Flash Lamp voltage in V"""
+        return self.__get_CAPVSET()
+
+    @flashlamp_voltage.setter
+    def flashlamp_voltage(self,voltage):
+        if isinstance(voltage,int):
+            return self.__set_CAPVSET(voltage)
+        else:
+            raise ValueError("Value must be an integer.")
+   
+    @property
+    def flashlamp_pulse_width(self):
+        """Flash Lamp Pulse Width in us"""
+        return self.__get_LPW()
+
+    @flashlamp_pulse_width.setter
+    def flashlamp_voltage(self,pulse_width):
+        if isinstance(voltage,int):
+            return self.__set_LPW(pulse_width)
+        else:
+            raise ValueError("Value must be an integer.")
+   
+    @property
+    def flashlamp_shots(self):
+        """Number of shots of the flashlamp, system-tracked."""
+        return self.__get_SSHOT()
+
+    ### We choose not to implement USHOT (User Shot Tracker) right now, although it could be useful to track the number of shots in a given experiment. I welcome contributions to the project. ;-)
+
+    @property
+    def flashlamp_trigger(self):
+        """Flashlamp trigger mode."""
+        trig = self.__get_TRIG()[0]
+        if trig == "I":
+            return "Internal"
+        else:
+            return "External"
+
+    @property
+    def qswitch_trigger(self):
+        """QSwitch trigger mode."""
+        trig = self.__get_TRIG()[1]
+        if trig == "I":
+            return "Internal"
+        else:
+            return "External"
+
+    def set_both_triggers(self,flashlamp_trig,qswitch_trig):
+        if flashlamp_trig == "Internal":
+            fl_int = True
+        elif flashlamp_trig == "External":
+            fl_int = False
+        else:
+            ValueError('Value must be "Internal" or "External".')
+
+        if qswitch_trig == "Internal":
+            qs_int = True
+        elif qswitch_trig == "External":
+            qs_int = False
+        else:
+            ValueError('Value must be "Internal" or "External".')
         
+        return self.__set_TRIG(fl_int,qs_int)
+
+    @flashlamp_trigger.setter
+    def flashlamp_trigger(self,trig):
+        # This is actually quite involved since both triggers are set at once. I will just write a helper code.
+        if trig == "Internal" or trig == "External":
+            self.set_both_triggers(trig,self.qswitch_trigger)
+        else:
+            raise ValueError('Value must be "Internal" or "External".')
+        
+    @qswitch_trigger.setter
+    def qswitch_trigger(self,trig):
+        # This is actually quite involved since both triggers are set at once. I will just write a helper code.
+        if trig == "Internal" or trig == "External":
+            self.set_both_triggers(self.flashlamp_trigger,trig)
+        else:
+            raise ValueError('Value must be "Internal" or "External".')
+
+    ## The QSPAR variables are exposed through system mode (Scan, Burst, F/N).
+    ## Note that F/N and Scan are basically the same (Continuous), but since the Q-Touch treats them differently, we will here as well.
+    ## We make the following distictions:
+    ##  - In Burst Mode when QSPAR1 != 0
+    ##  - In F/N mode when QSPAR3 == 1
+    ##  - In Scan mode otherwise
+
+
+    @property
+    def mode(self):
+        """
+        Returns the mode in which the laser is operating;
+        The QSPAR variables are exposed through system mode (Scan, Burst, F/N).
+        Note that F/N and Scan are basically the same (Continuous), but since the Q-Touch treats them differently, we will here as well.
+        We make the following distictions:
+        - In Burst Mode when QSPAR1 != 0
+        - In F/N mode when QSPAR3 == 1
+        - In Scan mode otherwise
+        """
+
+        qs1 = self.__get_QSPAR1()
+        qs2 = self.__get_QSPAR2()
+        qs3 = self.__get_QSPAR3()
+
+        if qs1 != 0:
+            return "Burst"
+        elif qs3 == 1:
+            return "F/N Mode"
+        else:
+            return "Scan"
+
+    def set_mode(self,mode,**mode_kwargs):
+        """
+        See also `mode`.
+
+        We can't really set QSPAR1-3 independently with this structure, so we have to set the mode and settings at the same time.
+
+        mode_kwargs:
+        Burst:
+        - cycles
+        - shots per cycle
+        - total length of cycle (in base freq)
+
+        Scan:
+        - shots per cycle
+        - total length of cycle (in base freq)
+
+        F/N:
+        - divider
+        """
+        if mode == "Burst":
+            if len(mode_kwargs) != 3:
+                raise ValueError("Setting mode to Burst requires 3 options. ('cycles','shots','total_length')")
+            if "cycles" not in mode_kwargs:
+                raise ValueError("Must set number of cycles ('cycles') in Burst mode.")
+            if "shots" not in mode_kwargs:
+                raise ValueError("Must set number of shots ('shots') in Burst mode.")
+            if "total_length" not in mode_kwargs:
+                raise ValueError("Must set total length of cycle ('total_length') in Burst mode.")
+            
+            cycles       = mode_kwargs["cycles"]
+            shots        = mode_kwargs["shots"]
+            total_length = mode_kwargs["total_length"]
+
+            if not isinstance(cycles,int):
+                raise ValueError("Value must be integer. ('cycles')")
+            if not isinstance(shots,int):
+                raise ValueError("Value must be integer. ('shots')")
+            if not isinstance(total_length,int):
+                raise ValueError("Value must be integer. ('total_length')")
+            
+            idle_shots = total_length-shots
+
+            if idle_shots < 0:
+                raise ValueError("Value of 'total_length' must be >= 'shots'.")
+
+            self.__set_QSPAR1(cycles)
+            self.__set_QSPAR2(total_length)
+            self.__set_QSPAR3(shots)
+
+        elif mode == "F/N Mode":
+            if len(mode_kwargs) != 2:
+                raise ValueError("Setting mode to F/N requires 1 option. ('divider')")
+            if "divider" not in mode_kwargs:
+                raise ValueError("Must set number of shots ('divider') in F/N mode.")
+            
+            divider      = mode_kwargs["divider"]
+
+            if not isinstance(divider,int):
+                raise ValueError("Value must be integer. ('shots')")
+
+            if divider < 1:
+                raise ValueError("Value of 'divider' must be >= '1'.")
+
+            self.__set_QSPAR1(0)
+            self.__set_QSPAR2(divider)
+            self.__set_QSPAR3(1)
+
+        elif mode == "Scan":
+            if len(mode_kwargs) != 2:
+                raise ValueError("Setting mode to Burst requires 2 options. ('shots','total_length')")
+            if "shots" not in mode_kwargs:
+                raise ValueError("Must set number of shots ('shots') in Burst mode.")
+            if "total_length" not in mode_kwargs:
+                raise ValueError("Must set total length of cycle ('total_length') in Burst mode.")
+            
+            shots        = mode_kwargs["shots"]
+            total_length = mode_kwargs["total_length"]
+
+            if not isinstance(shots,int):
+                raise ValueError("Value must be integer. ('shots')")
+            if not isinstance(total_length,int):
+                raise ValueError("Value must be integer. ('total_length')")
+            
+            idle_shots = total_length-shots
+
+            if idle_shots < 0:
+                raise ValueError("Value of 'total_length' must be >= 'shots'.")
+
+            self.__set_QSPAR1(0)
+            self.__set_QSPAR2(total_length)
+            self.__set_QSPAR3(shots)
+            pass
+        else:
+            raise ValueError('Value of "mode" must be "Burst", "F/N Mode" or "Scan".')
     
+    @property
+    def qswitch_delay(self):
+        """Q-Switch delay in ns"""
+        return self.__get_QDLY()
+
+    @qswitch_delay.setter
+    def qswitch_delay(self,delay):
+        if isinstance(delay,int):
+            if delay < 255 and delay >= 0:
+                return self.__set_QDLY(delay)
+            else:
+                raise ValueError("Value must be between 0 and 255.")
+        else:
+            raise ValueError("Value must be an integer.")
+
+    @property
+    def qswitch_sync_delay(self):
+        """Q-Switch sync output delay in ns"""
+        return self.__get_QDLYO()
+
+    @qswitch_sync_delay.setter
+    def qswitch_sync_delay(self,delay):
+        if isinstance(delay,int):
+            if delay < -500 and delay > 500:
+                return self.__set_QDLYO(delay)
+            else:
+                raise ValueError("Value must be between -500 and 500.")
+        else:
+            raise ValueError("Value must be an integer.")
+    ## Communication commands.
     def __get(self,command):
         try:
-            return self.lasercommunication.send_and_recv("PSVER")
+            return self.lasercommunication.send_and_recv(command)
         except:
             raise OSError ## Polish this
         
     def __set(self,command):
         try:
-            response = self.lasercommunication.send_and_recv("PSVER")
+            response = self.lasercommunication.send_and_recv(command)
             if "ERROR" not in response:
                 return 0
             else:
@@ -68,10 +313,10 @@ class LaserSettings:
             
     ## System commands
     def __get_PSVER(self):
-        full_response = self.__get("PSVER")
+        full_response = self.__get("PSVERS")
         if "ERROR" in full_response:
             raise IOError ## Flesh this out later
-        version       = float(full_response.split("PSVER = ")[1])
+        version       = float(full_response.split("PSVERS = ")[1])
         return version
         
     def __get_LVERS(self):
@@ -109,10 +354,13 @@ class LaserSettings:
         return response
         
     def __set_ECHO(self,value):
-        response = self.__set(f"ECHO {value}")
-        if response != 0:
-            raise IOError ## Flesh this out later
-        return response
+        if self.admin_mode:
+            response = self.__set(f"ECHO {value}")
+            if response != 0:
+                raise IOError ## Flesh this out later
+            return response
+        else:
+            raise RunetimeError("Trying to access a protected function while LaserSettings is not in admin mode.")
         
     ## State Command
     def __get_STATE(self,as_string = False):
@@ -180,7 +428,12 @@ class LaserSettings:
             return response
         else:
             raise RuntimeError("Trying to access a protected function while LaserSettings is not in admin mode.")
-            
+    def __get_TRIG(self):
+        full_response = self.__get("TRIG")
+        if "ERROR" in full_response:
+            raise Exception
+        return full_response.split("TRIG = ")[1]
+
     def __set_TRIG(self,flash_internal = True, q_internal=True):
         message = "TRIG "
         if flash_internal:
@@ -191,10 +444,10 @@ class LaserSettings:
             message+="I"
         else:
             message+="E"
-        response = self.__set(f"message")
+        response = self.__set(message)
         if response != 0:
             raise IOError ## Flesh this out later
-        return full_response
+        return response
   
     def __FL_RUN(self):
         response = self.__set(f"RUN")
